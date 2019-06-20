1ab39ca2348a7eae8eb387150be3921aae722735
DougSleighJr <dsleigh289@gmail.com>
2019-06-20 19:47:47 -0400

    added jquery

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Index.html cycling photo of children and family
+function cycle(img) {
+    $('.main-container').ready(function() {
+    var img = $('.kids-portrait');
+    img.delay(4000)
+    .queue(function(next) {img.attr('src', 'images/family.jpg'); next();} )
+    .delay(4000)
+    .queue(function(next) {img.attr('src', 'images/img_8954v2.jpg'); next();} )
+});
+}
+setInterval(() => {
+    cycle();
+}, 1000);
+
+//supportLeslie.html helping buttons
+$('.col-images + p').hide();
+
+$('.col-images').on('click', function() {
+    $(this).next().slideDown();
+});
+
+// $('col-images').on('mousover').css('cursor', 'pointer');
+
+
+
+    
